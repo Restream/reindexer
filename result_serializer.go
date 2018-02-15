4a4286f4b105287c08b45c8dab904ed2a71bc5ac
Oleg Gerasimov <ogerasimov@gmail.com>
2018-02-15 20:59:57 +0300

    [fix] wrong import path

--- result_serializer.go
+++ result_serializer.go
@@ -3,8 +3,8 @@ package reindexer
 import (
 	"fmt"
 
-	"git.itv.restr.im/itv-backend/reindexer/bindings"
-	"git.itv.restr.im/itv-backend/reindexer/cjson"
+	"github.com/restream/reindexer/bindings"
+	"github.com/restream/reindexer/cjson"
 )
 
 type rawResultItemParams struct {
