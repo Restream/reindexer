2d352ad6cf0c2be8269f576e954b5d17791d5fff
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-11 21:32:45 +0300

    gh actions; add pyrx workflow

--- .github/workflows/pyreindexer_workflow.sh
+++ .github/workflows/pyreindexer_workflow.sh
@@ -39,7 +39,7 @@ while true; do
 	run="$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPO/actions/runs/$run_id)"
 	echo $run
 	run_status="$(echo $run | jq '.status')"
-	echo 1 $run_status
+	echo 1 $run_status "$run_status" "completed"
 	if [[ "$run_status" == "completed" ]]; then
 		run_conclusion="$(echo $run | jq '.conclusion')"
 		echo 2 $run_conclusion
