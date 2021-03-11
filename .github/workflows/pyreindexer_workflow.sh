c72f4683bddd48afdd6e4fa8bee5c96c0ab14ea5
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-11 17:33:55 +0300

    gh actions; add pyrx workflow

---
+++ .github/workflows/pyreindexer_workflow.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+REPO=restream/reindexer-py
+USERNAME=reindexer-bot
+
+curl \
+  -u "${USERNAME}:${PYRX_GH_TOKEN}" \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${REPO}/actions/workflows/test-specified-rx.yml/dispatches \
+  -d '{"ref":"gh_actions", "inputs":{"rx_commit":"${GITHUB_SHA}"}}'
