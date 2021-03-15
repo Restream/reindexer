78ace3396e1fd5a5b424917a6c10a122302c1165
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-16 01:01:16 +0300

    [#711] - auth

--- .github/workflows/pyreindexer_workflow.sh
+++ .github/workflows/pyreindexer_workflow.sh
@@ -4,9 +4,10 @@ REPO=restream/reindexer-py
 USERNAME=reindexer-bot
 CHECK_TIMEOUT=10
 
-total_count=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPO/actions/workflows/test-specified-rx.yml/runs | jq '.total_count')
+total_count=$(curl -u "$USERNAME:$GITHUB_TOKEN" -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPO/actions/workflows/test-specified-rx.yml/runs | jq '.total_count')
 
 # workflow triggering
+# TODO change gh_actions to master before merge
 curl \
   -u "$USERNAME:$PYRX_GH_TOKEN" \
   -X POST \
@@ -17,7 +18,7 @@ curl \
 
 # find out workflow run id
 for i in {0..30}; do
-	all_runs=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPO/actions/workflows/test-specified-rx.yml/runs)
+	all_runs=$(curl -u "$USERNAME:$GITHUB_TOKEN" -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPO/actions/workflows/test-specified-rx.yml/runs)
 	new_total_count=$(echo $all_runs | jq '.total_count')
 	if [[ $new_total_count != $total_count ]]; then
 		run_id=$(echo $all_runs | jq '.workflow_runs | max_by(.run_number).id')
@@ -36,7 +37,7 @@ fi
 # workflow monitoring
 while true; do
   sleep ${CHECK_TIMEOUT}
-	run=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPO/actions/runs/$run_id)
+	run=$(curl -u "$USERNAME:$GITHUB_TOKEN" -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPO/actions/runs/$run_id)
 	run_status=$(echo $run | jq '.status' | sed '/"\(.*\)"/s//\1/')
 	if [ $run_status == "completed" ]; then
 		run_conclusion=$(echo $run | jq '.conclusion' | sed '/"\(.*\)"/s//\1/')
