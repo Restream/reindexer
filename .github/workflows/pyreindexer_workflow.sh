80f6a9b72729893672c18b8130c74c3efab1225b
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-04-10 20:45:21 +0300

    gh-actions

--- .github/workflows/pyreindexer_workflow.sh
+++ .github/workflows/pyreindexer_workflow.sh
@@ -13,7 +13,7 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/$REPO/actions/workflows/test-specified-rx.yml/dispatches \
-  -d "{\"ref\":\"gh_actions\", \"inputs\":{\"rx_commit\":\"$GITHUB_SHA\"}}"
+  -d "{\"ref\":\"master\", \"inputs\":{\"rx_commit\":\"$GITHUB_SHA\"}}"
 
 
 # find out workflow run id
