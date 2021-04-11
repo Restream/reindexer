c0be81572e3efe5569031b16eca5278fc6d4c043
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-04-11 15:59:20 +0300

    gh-actions

--- .github/workflows/install_grpc.sh
+++ .github/workflows/install_grpc.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/abseil/abseil-cpp.git
-cd abseil-cpp
-mkdir build && cd build
-cmake ..
-cmake --build . --target all
-cd ..
-rm -rf abseil-cpp
-
 git clone https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
@@ -16,5 +8,3 @@ cd cmake/build
 cmake -DCMAKE_BUILD_TYPE=Release -DgRPC_INSTALL=ON ../..
 make -j4
 sudo make install
-cd ..
-rm -rf grpc
