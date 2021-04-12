9ad2a45c4a654d16fdc8e218b023a3edccc1b8d5
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-04-12 20:31:37 +0300

    gh-actions

--- .github/workflows/install_gbench.sh
+++ .github/workflows/install_gbench.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/google/benchmark.git
 cd benchmark
 cmake -E make_directory "build"
-#cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
 cmake --build "build" --config Release
 sudo cmake --build "build" --config Release --target install
 cd ..
--- .github/workflows/install_grpc.sh
+++ .github/workflows/install_grpc.sh
@@ -6,5 +6,6 @@ git submodule update --init
 mkdir -p cmake/build
 cd cmake/build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DgRPC_INSTALL=ON ../..
-make -j4
-sudo make install
+cmake --build --config RelWithDebInfo
+sudo cmake --build --config RelWithDebInfo --target install
+cd ../../..
