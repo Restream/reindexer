0ada54e385cdc8e32f6af05d7eb31e68e5b7869a
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-04-12 20:08:35 +0300

    gh-actions

---
+++ .github/workflows/install_gbench.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/google/benchmark.git
+cd benchmark
+cmake -E make_directory "build"
+#cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake --build "build" --config Release
+sudo cmake --build "build" --config Release --target install
+cd ..
