7b9db02a93848393d024cf1f673e1deba583438f
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-04-13 17:24:24 +0300

    gh-actions

--- .github/workflows/install_grpc.sh
+++ .github/workflows/install_grpc.sh
@@ -5,7 +5,7 @@ cd grpc
 git submodule update --init
 mkdir -p cmake/build
 cd cmake/build
-cmake -D CMAKE_CXX_FLAGS="-lrt" -DCMAKE_BUILD_TYPE=Release DgRPC_INSTALL=ON ../..
+cmake -DCMAKE_BUILD_TYPE=Release DgRPC_INSTALL=ON ../..
 make -j4
 sudo make install
 cd ../../..
