e06b59dedc4dc2c314e7289b76c20fe43b7af606
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-05-31 21:35:36 +0300

    start

---
+++ .github/workflows/install_grpc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/grpc/grpc
+cd grpc
+git submodule update --init
+mkdir -p cmake/build
+cd cmake/build
+cmake -DCMAKE_BUILD_TYPE=Release DgRPC_INSTALL=ON ../..
+make -j4
+sudo make install
+sudo ldconfig
+cd ../../..
